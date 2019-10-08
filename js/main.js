c2941ee96a57aebb1436b39cf26540141c31a819
Mohnish Saini <sn.mohnish@gmail.com>
2019-10-09 00:10:16 +0800

    Changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 var app = angular.module("Kerboresclan_UI", []);
 
 
-app.controller("Controller1", function($scope) {
+app.controller("Controller1", function($scope, $http) {
   $scope.message = "Hello, AngularJS";	
+
+  $http({
+        method: "GET",
+        url: 'https://pythonflaskapp123.azurewebsites.net/api?input=Mohnish',
+        headers: {'Content-Type': 'application/json;charset=UTF-8'}
+
+  }).success(function(response) {
+      alert('Success');
+  }).error(function(response) {
+    alert('Error');
+  });
+
+
 }); 
