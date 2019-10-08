026dd449b45f61ea22f83a5bed12a601e39d3149
Mohnish Saini <sn.mohnish@gmail.com>
2019-10-09 00:59:58 +0800

    Changes

--- js/main.js
+++ js/main.js
@@ -10,9 +10,7 @@ app.controller("Controller1", function($scope, $http) {
         headers: {'Content-Type': 'application/json;charset=UTF-8'}
 
   }).success(function(response) {
-      alert('Success');
-      alert(response.input);
-      alert(response['input']);
+      $scope.messageFromServer = response.response.my_api_output
   }).error(function(response) {
     alert('Error');
   });
