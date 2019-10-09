1ada7c2344874eabbd73dd35f65a4dbee0231ae5
Mohnish Saini <sn.mohnish@gmail.com>
2019-10-09 11:03:56 +0800

    changes

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ app.config(['$locationProvider', '$routeProvider',
 function config($locationProvider, $routeProvider)  {
   $routeProvider.
     when('/main', { template: '<main-page></main-page>'}).
+    when('/dashboard', { template: '<dashboard-page></dashboard-page>'}).
     otherwise('/main');
 }
 
@@ -19,8 +20,9 @@ app.controller("Controller1", function($scope, $http, $window)  {
         headers: {'Content-Type': 'application/json;charset=UTF-8'}
 
   }).success(function(response) {
+      alert($scope.messageFromServer);
       $scope.messageFromServer = response.response.my_api_output;
-      $window.location.href = '/dashboard.html';
+      //$window.location.href = '/dashboard.html';
   }).error(function(response) {
     alert('Error');
   });
