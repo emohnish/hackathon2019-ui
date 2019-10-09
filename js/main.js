a8663192942b982eee7711ae91a0b49f3f8f173a
Mohnish Saini <sn.mohnish@gmail.com>
2019-10-09 10:30:17 +0800

    changes

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ app.controller("Controller1", function($scope, $http) {
         headers: {'Content-Type': 'application/json;charset=UTF-8'}
 
   }).success(function(response) {
-      $scope.messageFromServer = response.response.my_api_output
+      $scope.messageFromServer = response.response.my_api_output;
+      $window.location.href = '/dashboard.html';
   }).error(function(response) {
     alert('Error');
   });
