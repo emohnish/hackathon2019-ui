cd46cdfcd0f369901988306919425b64324084e8
Mohnish Saini <sn.mohnish@gmail.com>
2019-10-09 11:31:00 +0800

    changes

--- js/main.js
+++ js/main.js
@@ -12,11 +12,12 @@ function config($locationProvider, $routeProvider)  {
 ]);
 
 app.controller("Controller1", function($scope, $http, $window, $location)  {
-  $scope.message = "Hello, AngularJS";	
+  $scope.message = "Hello, AngularJS";
+  $scope.loginpage = {};	
 
   $scope.signIn = function() {
-    alert($scope.userName);
-    alert($scope.password);
+    alert($scope.loginpage.userName);
+    alert($scope.loginpage.password);
   }
 
   $http({
